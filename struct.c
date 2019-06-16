1ff473ca88fcfbf43524cf5a79cfdc9adef28d8e
unknown <mjdepuy@gmail.com>
2019-06-16 22:51:03 +0000

    Fixed issue in struct.c

--- struct.c
+++ struct.c
@@ -3,14 +3,15 @@
 
 struct OBJ{
     int i;
-    char j[32];
-}
+    char *j;
+};
 
 int main(int argc, char **argv){
     struct OBJ a;
     a.i = 109;
-    a.j = "Struct test string!\n";
-
+    a.j = (char *)malloc(sizeof(char)*32);
+    a.j = "Test string!";
+    
     printf("Printing struct member values...\n");
     printf("a.i = %d\n",a.i);
     printf("a.j = '%s'\n",a.j);
